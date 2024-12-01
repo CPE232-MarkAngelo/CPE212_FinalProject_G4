0f2cf059880fb9260e95697b54a764d950192cb6
Mark Angelo Aquino <qmaaaquino@tip.edu.ph>
2024-12-01 18:06:09 +0800

    Final_Project

---
+++ Docker/Group4.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+sample = Flask(__name__)
+
+@sample.route("/")
+def main():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    sample.run(host="0.0.0.0", port=5000, debug=True)
+
+
+
+
